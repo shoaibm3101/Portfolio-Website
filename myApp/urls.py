ad01a882b32d1ddc65c93bc7a111e7cc731bb502
shoaibm3101 <shoaibmtoronto@gmail.com>
2025-09-01 15:50:38 -0400

    Setting Up Static File

--- myApp/urls.py
+++ myApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.homepage, name="home")
 ]
+
+urlpatterns += staticfiles_urlpatterns()

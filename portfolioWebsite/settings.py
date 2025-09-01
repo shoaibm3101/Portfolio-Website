f95b5d59dc56343d32380eba7609567400904bc7
shoaibm3101 <shoaibmtoronto@gmail.com>
2025-09-01 15:59:05 -0400

    Small Static Files Change

--- portfolioWebsite/settings.py
+++ portfolioWebsite/settings.py
@@ -132,6 +132,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
